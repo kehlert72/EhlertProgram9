71e6b091d2716f79baf60c283ee1afe71068d749
kehlert72 <kaleb.ehlert@gmail.com>
2020-07-27 17:19:48 -0400

    Completed project

---
+++ Calculator.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace EhlertProgram9
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCommandLine = new System.Windows.Forms.TextBox();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnPosOrNeg = new System.Windows.Forms.Button();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum0 = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnMultiplication = new System.Windows.Forms.Button();
+            this.btnSubtraction = new System.Windows.Forms.Button();
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCommandLine
+            // 
+            this.txtCommandLine.Enabled = false;
+            this.txtCommandLine.Location = new System.Drawing.Point(12, 12);
+            this.txtCommandLine.Name = "txtCommandLine";
+            this.txtCommandLine.Size = new System.Drawing.Size(260, 20);
+            this.txtCommandLine.TabIndex = 0;
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.Location = new System.Drawing.Point(23, 51);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(63, 23);
+            this.btnNum1.TabIndex = 1;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = true;
+            this.btnNum1.Click += new System.EventHandler(this.btnNum1_Click);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.Location = new System.Drawing.Point(23, 80);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(63, 23);
+            this.btnNum4.TabIndex = 2;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = true;
+            this.btnNum4.Click += new System.EventHandler(this.btnNum4_Click);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.Location = new System.Drawing.Point(92, 80);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(63, 23);
+            this.btnNum5.TabIndex = 3;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = true;
+            this.btnNum5.Click += new System.EventHandler(this.btnNum5_Click);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.Location = new System.Drawing.Point(92, 51);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(63, 23);
+            this.btnNum2.TabIndex = 4;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = true;
+            this.btnNum2.Click += new System.EventHandler(this.btnNum2_Click);
+            // 
+            // btnPosOrNeg
+            // 
+            this.btnPosOrNeg.Location = new System.Drawing.Point(23, 138);
+            this.btnPosOrNeg.Name = "btnPosOrNeg";
+            this.btnPosOrNeg.Size = new System.Drawing.Size(63, 23);
+            this.btnPosOrNeg.TabIndex = 5;
+            this.btnPosOrNeg.Text = "+/-";
+            this.btnPosOrNeg.UseVisualStyleBackColor = true;
+            this.btnPosOrNeg.Click += new System.EventHandler(this.btnPosOrNeg_Click);
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.Location = new System.Drawing.Point(23, 109);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(63, 23);
+            this.btnNum7.TabIndex = 6;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = true;
+            this.btnNum7.Click += new System.EventHandler(this.btnNum7_Click);
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.Location = new System.Drawing.Point(92, 109);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(63, 23);
+            this.btnNum8.TabIndex = 7;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = true;
+            this.btnNum8.Click += new System.EventHandler(this.btnNum8_Click);
+            // 
+            // btnNum0
+            // 
+            this.btnNum0.Location = new System.Drawing.Point(92, 138);
+            this.btnNum0.Name = "btnNum0";
+            this.btnNum0.Size = new System.Drawing.Size(63, 23);
+            this.btnNum0.TabIndex = 8;
+            this.btnNum0.Text = "0";
+            this.btnNum0.UseVisualStyleBackColor = true;
+            this.btnNum0.Click += new System.EventHandler(this.btnNum0_Click);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.Location = new System.Drawing.Point(161, 51);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(63, 23);
+            this.btnNum3.TabIndex = 9;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = true;
+            this.btnNum3.Click += new System.EventHandler(this.btnNum3_Click);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.Location = new System.Drawing.Point(161, 109);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(63, 23);
+            this.btnNum9.TabIndex = 10;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = true;
+            this.btnNum9.Click += new System.EventHandler(this.btnNum9_Click);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.Location = new System.Drawing.Point(161, 80);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(63, 23);
+            this.btnNum6.TabIndex = 11;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = true;
+            this.btnNum6.Click += new System.EventHandler(this.btnNum6_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(161, 138);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(63, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(230, 51);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(42, 23);
+            this.btnDivision.TabIndex = 14;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnMultiplication
+            // 
+            this.btnMultiplication.Location = new System.Drawing.Point(230, 80);
+            this.btnMultiplication.Name = "btnMultiplication";
+            this.btnMultiplication.Size = new System.Drawing.Size(42, 23);
+            this.btnMultiplication.TabIndex = 15;
+            this.btnMultiplication.Text = "*";
+            this.btnMultiplication.UseVisualStyleBackColor = true;
+            this.btnMultiplication.Click += new System.EventHandler(this.btnMultiplication_Click);
+            // 
+            // btnSubtraction
+            // 
+            this.btnSubtraction.Location = new System.Drawing.Point(230, 138);
+            this.btnSubtraction.Name = "btnSubtraction";
+            this.btnSubtraction.Size = new System.Drawing.Size(42, 23);
+            this.btnSubtraction.TabIndex = 16;
+            this.btnSubtraction.Text = "-";
+            this.btnSubtraction.UseVisualStyleBackColor = true;
+            this.btnSubtraction.Click += new System.EventHandler(this.btnSubtraction_Click);
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.Location = new System.Drawing.Point(230, 109);
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.Size = new System.Drawing.Size(42, 23);
+            this.btnAddition.TabIndex = 17;
+            this.btnAddition.Text = "+";
+            this.btnAddition.UseVisualStyleBackColor = true;
+            this.btnAddition.Click += new System.EventHandler(this.btnAddition_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Location = new System.Drawing.Point(230, 167);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(42, 23);
+            this.btnEquals.TabIndex = 18;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnAddition);
+            this.Controls.Add(this.btnSubtraction);
+            this.Controls.Add(this.btnMultiplication);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnNum6);
+            this.Controls.Add(this.btnNum9);
+            this.Controls.Add(this.btnNum3);
+            this.Controls.Add(this.btnNum0);
+            this.Controls.Add(this.btnNum8);
+            this.Controls.Add(this.btnNum7);
+            this.Controls.Add(this.btnPosOrNeg);
+            this.Controls.Add(this.btnNum2);
+            this.Controls.Add(this.btnNum5);
+            this.Controls.Add(this.btnNum4);
+            this.Controls.Add(this.btnNum1);
+            this.Controls.Add(this.txtCommandLine);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCommandLine;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnPosOrNeg;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button btnNum0;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnMultiplication;
+        private System.Windows.Forms.Button btnSubtraction;
+        private System.Windows.Forms.Button btnAddition;
+        private System.Windows.Forms.Button btnEquals;
+    }
+}
+
---
+++ Calculator.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*  ID
+ *      Calculator, Kaleb Ehlert, 4/1/2018
+ *  Purpose
+ *      The purpose of the program is to implement a calculator that can take the input of integers/integrals and output the result
+ *      of the operation.
+ *  Form Controls
+ *      txtCommandLine: The textfield that displays the results of the operation and displays the input as it is being typed.
+ *      btnNum1: The button that inputs the number one in the calculator
+ *      btnNum2: The button that inputs the number two in the calculator
+ *      btnNum3: The button that inputs the number three in the calculator
+ *      btnNum4: The button that inputs the number four in the calculator
+ *      btnNum5: The button that inputs the number five in the calculator
+ *      btnNum6: The button that inputs the number six in the calculator
+ *      btnNum7: The button that inputs the number seven in the calculator
+ *      btnNum8: The button that inputs the number eight in the calculator
+ *      btnNum9: The button that inputs the number nine in the calculator
+ *      btnNum0: The button that inputs the number zero in the calculator
+ *      btnPosOrNeg: The button that changes the value based on whether it is positive or negative only if you have not typed in the
+ *      number yet.
+ *      btnClear: The button that clears all operations.
+ *      btnDivision: The button that enters in the divison operation.
+ *      btnMultiplication: The button that enters in the multiplication operation.
+ *      btnAddition: The button that enters in the addition operation.
+ *      btnSubtraction: The button that enters in the subtraction operation.
+ *      btnEquals: The button that performs the operation.
+ *  Code & Logic
+ *      The input is the buttons on the screen that the user can click.
+ *      The processing takes two operands and performs a math operation on them.
+ *      The output is the result of the math operations performed.
+ */
+
+namespace EhlertProgram9
+{
+    public partial class Calculator : Form
+    {
+        Boolean equalsClicked;
+
+        int result;
+        int operand1;
+        int operand2;
+
+        string input;
+        string operation;
+
+        public Calculator()
+        {
+            InitializeComponent();
+
+            equalsClicked = false;
+
+            result = 0;
+            operand1 = 0;
+            operand2 = 0;
+
+            input = "";
+            operation = "";
+        }
+
+        private void btnNum1_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "1";
+            txtCommandLine.Text += "1";
+        }
+
+        private void btnNum2_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "2";
+            txtCommandLine.Text += "2";
+        }
+
+        private void btnNum3_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "3";
+            txtCommandLine.Text += "3";
+        }
+
+        private void btnNum4_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "4";
+            txtCommandLine.Text += "4";
+        }
+
+        private void btnNum5_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "5";
+            txtCommandLine.Text += "5";
+        }
+
+        private void btnNum6_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "6";
+            txtCommandLine.Text += "6";
+        }
+
+        private void btnNum7_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "7";
+            txtCommandLine.Text += "7";
+        }
+
+        private void btnNum8_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "8";
+            txtCommandLine.Text += "8";
+        }
+
+        private void btnNum9_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "9";
+            txtCommandLine.Text += "9";
+        }
+
+        private void btnNum0_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            input += "0";
+            txtCommandLine.Text += "0";
+        }
+
+        private void btnPosOrNeg_Click(object sender, EventArgs e)
+        {
+            if (equalsClicked == true) {
+                txtCommandLine.Text = "";
+                equalsClicked = false;
+            }
+
+            if (input == "-")
+                input = "";
+            else
+                input += "-";
+
+            txtCommandLine.Text += "-";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCommandLine.Text = "";
+            Clear();
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            operandAssignment();
+
+            operation = "/";
+
+            txtCommandLine.Text += " / ";
+        }
+
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            operandAssignment();
+
+            operation = "*";
+
+            txtCommandLine.Text += " * ";
+        }
+
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            operandAssignment();
+
+            operation = "+";
+
+            txtCommandLine.Text += " + ";
+        }
+
+        private void btnSubtraction_Click(object sender, EventArgs e)
+        {
+            operandAssignment();
+
+            operation = "-";
+
+            //operationClicked = true;
+
+            txtCommandLine.Text += " - ";
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            equalsClicked = true;
+
+            operandAssignment();
+
+            // Display the result
+            txtCommandLine.Text = result.ToString();
+
+            // Clear all variables
+            Clear();
+        }
+
+        // Assigns an operand a value based off of what operand is filled or not
+        private void operandAssignment()
+        {
+            if (operand1 == 0) {
+                operand1 = int.Parse(input);
+                input = "";
+
+                operandCalculation();
+            } else {
+                operand2 = int.Parse(input);
+                input = "";
+
+                operandCalculation();
+            }
+        }
+
+        // Performs the math on the two operands
+        private void operandCalculation()
+        {
+            if (operand1 != 0 && operand2 != 0) {
+                if (operation == "/") {
+                    try {
+                        result = operand1 / operand2;
+                    } catch {
+                        MessageBox.Show("Error! Divide by zero exception.");
+                    }
+
+                    operation = "";
+                    operand1 = result;
+                    operand2 = 0;
+                }
+                else if (operation == "*") {
+                    result = operand1 * operand2;
+
+                    operation = "";
+                    operand1 = result;
+                    operand2 = 0;
+                }
+                else if (operation == "+") {
+                    result = operand1 + operand2;
+
+                    operation = "";
+                    operand1 = result;
+                    operand2 = 0;
+                }
+                else if (operation == "-") {
+                    result = operand1 - operand2;
+
+                    operation = "";
+                    operand1 = result;
+                    operand2 = 0;
+                }
+            }
+        }
+
+        // Clears all variables as it is only called when the enter button is clicked
+        private void Clear()
+        {
+            result = 0;
+            operand1 = 0;
+            operand2 = 0;
+
+            equalsClicked = false;
+
+            input = "";
+        }
+    }
+}
